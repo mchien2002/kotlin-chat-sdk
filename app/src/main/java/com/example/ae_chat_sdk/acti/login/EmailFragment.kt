2fbd48826d6a11036ea0a73aec54e379733175df
TRANHUYNHTANPHA\tranh <tranhuynhtanphat9380@gmail.com>
2023-02-12 15:31:01 +0700

    UI Intro and Login

--- app/src/main/java/com/example/ae_chat_sdk/acti/login/EmailFragment.kt
+++ app/src/main/java/com/example/ae_chat_sdk/acti/login/EmailFragment.kt
@@ -1,20 +1,17 @@
 package com.example.ae_chat_sdk.acti.login
 
 
-import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
-import android.util.Log
 import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.EditText
 import androidx.navigation.findNavController
 import com.example.ae_chat_sdk.MainActivity
 import com.example.ae_chat_sdk.R
@@ -25,20 +22,26 @@ import com.google.android.material.textfield.TextInputLayout
 class EmailFragment : Fragment(), View.OnClickListener, TextWatcher {
 
     lateinit var btnSendEmail: Button
-    lateinit var editEmail:TextInputEditText
-    lateinit var layoutEditEmail:TextInputLayout
+
+    lateinit var editEmail: TextInputEditText
+
+    lateinit var layoutEditEmail: TextInputLayout
+
+    lateinit var inputEmailContainer: ViewGroup
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val v:View = inflater.inflate(R.layout.fragment_email, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_email, container, false)
 
         // Declare
-        btnSendEmail=v.findViewById<Button>(R.id.buttonVerify)
-        editEmail=v.findViewById<TextInputEditText>(R.id.editEmail)
-        layoutEditEmail=v.findViewById<TextInputLayout>(R.id.input_email_layout)
+        inputEmailContainer = v.findViewById<ViewGroup>(R.id.inputEmailTransition)
+        btnSendEmail = inputEmailContainer.findViewById<Button>(R.id.buttonVerify)
+        editEmail = inputEmailContainer.findViewById<TextInputEditText>(R.id.editEmail)
+        layoutEditEmail = inputEmailContainer.findViewById<TextInputLayout>(R.id.inputEmailLayout)
 
         // Listen
         btnSendEmail.setOnClickListener(this)
@@ -47,8 +50,8 @@ class EmailFragment : Fragment(), View.OnClickListener, TextWatcher {
     }
 
     override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.buttonVerify->{
+        when (v?.id) {
+            R.id.buttonVerify -> {
                 v.findNavController().navigate(R.id.action_emailFragment_to_OTPFragment)
             }
         }
@@ -63,21 +66,23 @@ class EmailFragment : Fragment(), View.OnClickListener, TextWatcher {
     }
 
     override fun afterTextChanged(s: Editable?) {
-        MainActivity.Email=s.toString().trim()
+        MainActivity.Email = s.toString().trim()
 
         fun String.isValidEmail() =
-            !TextUtils.isEmpty(MainActivity.Email) && Patterns.EMAIL_ADDRESS.matcher(MainActivity.Email).matches()
+            !TextUtils.isEmpty(MainActivity.Email) && Patterns.EMAIL_ADDRESS.matcher(MainActivity.Email)
+                .matches()
 
-        if(!MainActivity.Email.isValidEmail()){
-            layoutEditEmail.error="Email chưa đúng cú pháp!"
-        }
-        else{
-            layoutEditEmail.isErrorEnabled=false
-            btnSendEmail.visibility=View.VISIBLE
+
+        if (!MainActivity.Email.isValidEmail()) {
+            layoutEditEmail.error = "Email chưa đúng cú pháp!"
+            btnSendEmail.visibility = View.GONE
+        } else {
+            layoutEditEmail.isErrorEnabled = false
+            btnSendEmail.visibility = View.VISIBLE
         }
     }
 
-    fun setButtonEnable(btn:MaterialButton){
+    fun setButtonEnable(btn: MaterialButton) {
         btn.setBackgroundColor(Color.parseColor("#FFFFB800"))
     }
 }
