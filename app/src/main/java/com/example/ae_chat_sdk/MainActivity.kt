291661e31476c58c2d84d3ba073002e772a3c05c
TRANHUYNHTANPHA\tranh <tranhuynhtanphat9380@gmail.com>
2023-02-16 11:37:51 +0700

    UI

--- app/src/main/java/com/example/ae_chat_sdk/MainActivity.kt
+++ app/src/main/java/com/example/ae_chat_sdk/MainActivity.kt
@@ -8,15 +8,17 @@ import com.example.ae_chat_sdk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
-        var Email:String =""
+    companion object {
+        var Email: String = ""
+        var OTP: String = ""
     }
-//    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    //    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.fragment_home)
+//        setContentView(R.layout.fragment_otp)
 
         this.binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
--- app/src/main/java/com/example/ae_chat_sdk/acti/intro/IntroFragment.kt
+++ app/src/main/java/com/example/ae_chat_sdk/acti/intro/IntroFragment.kt
@@ -8,15 +8,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.core.view.marginTop
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.ae_chat_sdk.R
 
-class IntroFragment : Fragment(){
+class IntroFragment : Fragment() {
 
-    private lateinit var btnSignIn: Button
+
+    private lateinit var ic: ImageView
+    lateinit var layoutItr: LinearLayout
+    lateinit var logo: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +32,29 @@ class IntroFragment : Fragment(){
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_intro, container, false)
+        ic = v.findViewById<ImageView>(R.id.iconLogo)
+        layoutItr = v.findViewById<LinearLayout>(R.id.layoutItro)
+        logo = v.findViewById<ImageView>(R.id.logo)
+
+//        Handler(Looper.getMainLooper()).postDelayed({
+//
+//        }, 2700)
+
         Handler(Looper.getMainLooper()).postDelayed({
+            logo.setBackgroundResource(R.drawable.logo2)
+            layoutItr.setBackgroundResource(R.color.color2)
+
+        }, 2200)
+
+
+        ic.animate().alpha(0F).setDuration(200).setStartDelay(2000)
+        logo.animate().translationY(-1550F).setDuration(500).setStartDelay(2000)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+//            layoutInputEmail.animate().translationY(-1850F).setDuration(200).setStartDelay(2500)
             v.findNavController().navigate(R.id.action_introFragment_to_emailFragment)
-        }, 3000)
+        }, 2600)
         return v;
     }
 
