d6fb03b60e8e51e23b6a160f0836f985cf24df9e
TranHuynhTanPhat <tranhuynhtanphat9380@gmail.com>
2023-05-09 22:34:38 +0700

    Phat update UI

--- app/src/main/java/com/example/ae_chat_sdk/data/socket/SocketConstant.kt
+++ app/src/main/java/com/example/ae_chat_sdk/data/socket/SocketConstant.kt
@@ -2,6 +2,6 @@ package com.example.ae_chat_sdk.data.socket
 
 class SocketConstant {
     companion object{
-        const val URL:String="ws://app-4fb62ee8-8d1c-4072-8838-ae07a2df04ce.cleverapps.io/socket-connection/ws"
+        const val URL:String="ws://app-539ccd9b-9361-41a7-8adb-f7d64732641c.cleverapps.io/socket-connection/ws"
     }
 }
